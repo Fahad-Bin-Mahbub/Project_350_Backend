04f7526e7f570b6bc28dd205f5f38e4c9579912f
Fahad Bin Mahbub <fahadbm3234@gmail.com>
2024-04-02 01:46:48 +0600

    changed

--- config/db.js
+++ config/db.js
@@ -11,7 +11,7 @@ mongoose
 	.connect(process.env.MONGODB_URI, config)
 	.then(() => console.log(`Database is connected`.green))
 	.catch((e) => console.log(`${e.message}`.red.bold));
-
+console.log(process.env.MONGODB_URI);
 const store = MongoStore.create({ mongoUrl: process.env.MONGODB_URI });
 
 module.exports = store;
