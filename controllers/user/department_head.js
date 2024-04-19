36a3b16edbdc4795716d172e477aaf0d62cf2cc3
Md.Shibly Rahman Alve <alverohman2000@gmail.com>
2024-04-19 09:43:17 +0600

    added more routes

--- controllers/user/department_head.js
+++ controllers/user/department_head.js
@@ -2,6 +2,7 @@ const asyncHandler = require("../../middlewares/asyncHandler");
 const ErrorResponse = require("../../utils/errorResponse");
 const User = require("../../models/user");
 const Ci = require("../../models/user/ci");
+const Department = require("../../models/department");
 
 exports.createCi = asyncHandler(async (req, res, next) => {
     const { session, email } = req.body;
@@ -26,5 +27,32 @@ exports.createCi = asyncHandler(async (req, res, next) => {
     }catch(error){
         console.log(error);
         return next(new ErrorResponse("Ci not added", 500));
+    }
+});
+
+exports.getAllCi = asyncHandler(async (req, res, next) => {
+    try {
+        const cis = await Ci.find();
+        return res.status(200).json({
+            success:true,
+            data:cis
+        });
+    } catch (error) {
+        console.log(error);
+        return next(new ErrorResponse("Could not fetch Ci", 500));
+    }
+});
+
+exports.getDept = asyncHandler(async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const dept = await Department.findById(id);
+        return res.status(200).json({
+            success:true,
+            data:dept
+        });
+    } catch (error) {
+        console.log(error);
+        return next(new ErrorResponse("Could not fetch dept", 500));
     }
 });
