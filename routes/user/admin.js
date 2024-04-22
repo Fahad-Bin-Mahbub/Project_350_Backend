700c798ca09991f8d529e5cd5e75cd3efe14049e
Rabbani2019331039 <fathoor39@student.sust.edu>
2024-04-23 00:35:05 +0600

    ci multiprotect added

--- routes/user/admin.js
+++ routes/user/admin.js
@@ -3,23 +3,28 @@ const router = express.Router();
 
 const controller = require("../../controllers/user/admin");
 
-const { adminProtect,multiProtect } = require("../../middlewares/authProtect");
+const { adminProtect, multiProtect } = require("../../middlewares/authProtect");
 
-router.post("/create",controller.createAdmin);
-router.post("/login",controller.adminLogin);
-router.post("/logout",controller.adminLogout);
-router.post("/create-department",adminProtect,controller.createDepartment);
-router.get("/get-department-id/:name",adminProtect,controller.getDepartmentId);
+router.post("/create", controller.createAdmin);
+router.post("/login", controller.adminLogin);
+router.post("/logout", controller.adminLogout);
+router.post("/create-department", adminProtect, controller.createDepartment);
+router.get(
+	"/get-department-id/:name",
+	adminProtect,
+	controller.getDepartmentId
+);
 router.get(
 	"/get-all-teachers",
 	multiProtect(["admin", "department_head", "ci"]),
 	controller.getAllTeachers
 );
-router.get("/get-all-departments",adminProtect,controller.getAllDepartments);
+router.get("/get-all-departments", adminProtect, controller.getAllDepartments);
 router.get(
 	"/teachers/:departmentId",
-	multiProtect(["admin", "department_head"]),controller.getAllByDepartment
+	multiProtect(["admin", "department_head", "ci"]),
+	controller.getAllByDepartment
 );
-router.put("/make-head/:userId",adminProtect,controller.makeHead)
-router.put("/remove-head/:userId", adminProtect,controller.removeHead);
+router.put("/make-head/:userId", adminProtect, controller.makeHead);
+router.put("/remove-head/:userId", adminProtect, controller.removeHead);
 module.exports = router;
